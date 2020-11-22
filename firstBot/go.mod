025406502651303ef6b72a134ed76ea45e05d006
Zoran-Bailey <s3775574@student.rmit.edu.au>
2020-11-22 22:09:36 +1100

    Initial commit

---
+++ firstBot/go.mod
@@ -0,0 +1,5 @@
+module firstBot
+
+go 1.14
+
+require github.com/bwmarrin/discordgo v0.22.0
---
+++ firstBot/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	clientID     = "779966919733542952"
+	clientSecret = "yrre588AZavqZmdCr35GUp52jJcELZju"
+)
+
+func main() {
+	discord, err := discordgo.New("Bot " + clientSecret)
+	// handle discord client creation failure
+	if err != nil {
+		fmt.Println("Failed to initalize a discord client -> ", err)
+		return
+	}
+
+	discord.AddHandler(messageCreate)
+
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.Message) {
+
+}
